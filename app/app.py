6af45d9bbafa796f2c729b21c12b732ff5ec5ec3
Xin <Xin.wamg@mewhouse.com.au>
2025-05-06 22:21:12 +0800

    Modify the path after login and the home page after login

--- app/app.py
+++ app/app.py
@@ -46,7 +46,7 @@ def login():
         user = User.query.filter_by(username=request.form["username"]).first()
         if user and check_password_hash(user.password, request.form["password"]):
             login_user(user)
-            return redirect(url_for("dashboard"))
+            return redirect(url_for("home"))
         flash("Invalid credentials")
     return render_template("login.html")
 
